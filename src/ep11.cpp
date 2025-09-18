62e2bf0d122548e6f0c93501f5190df752eed2e4
Nia M <unikorn.nkm31@gmail.com>
2025-09-18 18:48:02 -0400

    reorganizing files

---
+++ src/ep11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    //this is a sliding window problem
+    //need to do a sliding window of 4 each time
+    // horizontal should be trivial to code
+    //vertical could be trickier
+    // diagonal will be the most tricky
+
+}
